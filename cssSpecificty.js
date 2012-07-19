1ab03afa6794bdc2c9cf07648a9c07f8f2c23b66
Tingan Ho <tingan@clockies.com>
2012-07-20 01:37:53 +0200

    Added some javscript file

--- cssSpecificty.js
+++ cssSpecificty.js
@@ -5,20 +5,18 @@ var Specificity = (function(){
     var C = {};
     
     // CSS Pseudo Classes http://www.w3.org/TR/CSS2/selector.html#link-pseudo-classes & http://reference.sitepoint.com/css/pseudoclasses
-    var pseudoClassesCSS2 = 
-    [':first-child', ':link', ':visitied', ':hover', ':active', ':focus', ':lang'];
+    var pseudoClassesCSS2 = [':first-child', ':link', ':visitied', ':hover', ':active', ':focus', ':lang'];
     
     // CSS3 Pseudo Classes http://reference.sitepoint.com/css/pseudoclasses
     var pseudoClassesCSS3 = 
-    [':nth-child', ':nth-last-child', ':nth-of-type', ':nth-of-last-type',
-     ':first-of-type', ':last-of-type', ':only-child', ':only-of-type',  ':root', ':empty', 
-     ':target', ':enabled', ':disbaled', ':checked', ':not'];
-
+        [':nth-child', ':nth-last-child', ':nth-of-type', ':nth-of-last-type', 
+         ':first-of-type', ':last-of-type', ':only-child', ':only-of-type',  
+         ':root', ':empty', ':target', ':enabled', ':disbaled', ':checked', ':not'];
+    
     const pseudoClasses = pseudoClassesCSS2.concat(pseudoClassesCSS3);
 
     // Pseudo Elements http://reference.sitepoint.com/css/pseudoelements
     const pseudoElements = ['first-line', 'first-letter', 'before', 'after', ':selection'];
-    
     
     
     function numMatches(selector, regex) {
@@ -31,13 +29,12 @@ var Specificity = (function(){
       return typeof selector == "string" ? matchesSelector.call(element, selector) : selector === element;
     }
     
-    C.getBiggestPoint = function(points) {
-        var points = sortPoints(points);
+    function getBiggestPoint(points) {
+        var points = C.sortPoints(points);
         return points[0];
     }
     
     C.sortPoints = function(points) {
-
         points.sort(function(a, b) {
             if(a[0] > b[0])
                 return -1;
@@ -58,7 +55,10 @@ var Specificity = (function(){
         return points;
     }
     
-    C.specificity = function(selector, element) {
+    C.getSpecificity = function(selector, element) {
+    
+    
+    
     
         var splittedSelector = selector.split(',');
         var points = [];
@@ -86,6 +86,7 @@ var Specificity = (function(){
         return getBiggestPoint(points);
     }
     
+    
     C.constructor();
     
     return C;
@@ -99,6 +100,4 @@ var Specificity = (function(){
 var element = document.createElement('div');
 element.className = 'hello juoo';
 
-document.write(JSON.stringify(specificity("", element)));
-    
-​
+document.write(JSON.stringify(Specificity.getSpecificity("html, body", element)));
